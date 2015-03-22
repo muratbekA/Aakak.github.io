86ba29c386b0055cbaa555ace5dfdf2d565ff3d1
Aakak <akak@Akaks-MacBook-Air.local>
2015-03-22 12:43:44 -0700

    fixing map

--- js/main.js
+++ js/main.js
@@ -181,7 +181,7 @@ function init() {
     // Basic options for a simple Google Map
     // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
 
-	    var myLatLng = new google.maps.LatLng(22.402789, 91.822156);
+	    var myLatLng = new google.maps.LatLng(42.864866, 74.548454);
 
 	    var mapOptions = {
 	        zoom: 15,
@@ -253,7 +253,7 @@ function init() {
 
     // Let's also add a marker while we're at it
     var marker = new google.maps.Marker({
-        position: new google.maps.LatLng(22.402789, 91.822156),
+        position: new google.maps.LatLng(42.864866, 74.548454),
         map: map,
 		icon: 'img/icons/map-marker.png',
     });
